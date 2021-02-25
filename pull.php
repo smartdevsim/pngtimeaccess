a9f09f92a6af26dec930ef2a922fa370f4af5ff5
makspavlovskyi <empiremaksik@gmail.com>
2021-02-25 09:05:27 +0800

    fff

--- pull.php
+++ pull.php
@@ -2,7 +2,7 @@
 
 // Use in the “Post-Receive URLs” section of your GitHub repo.
     $content = "some text here";
-    $fp = fopen("/myText.txt","wb");
+    $fp = fopen("myText.txt","wb");
     fwrite($fp,$content);
     fclose($fp);
     if ( $_POST['payload'] ) {
