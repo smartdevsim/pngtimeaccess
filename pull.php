687f53a69b6f8195a5efc7f69985c8c36c6ed5c1
makspavlovskyi <empiremaksik@gmail.com>
2021-02-25 09:02:24 +0800

    test

--- pull.php
+++ pull.php
@@ -1,16 +1,16 @@
 <?php
 
 // Use in the “Post-Receive URLs” section of your GitHub repo.
-$content = "some text here";
-    $fp = fopen($_SERVER['DOCUMENT_ROOT'] . "/myText.txt","wb");
-    fwrite($fp,$content);
-    fclose($fp);
-if ( $_POST['payload'] ) {
     $content = "some text here";
     $fp = fopen($_SERVER['DOCUMENT_ROOT'] . "/myText.txt","wb");
     fwrite($fp,$content);
     fclose($fp);
-    shell_exec( 'cd /var/www/html/pngtimeaccess && git reset –hard HEAD && git pull' );
-}
+    if ( $_POST['payload'] ) {
+        $content = "some text here";
+        $fp = fopen($_SERVER['DOCUMENT_ROOT'] . "/myText.txt","wb");
+        fwrite($fp,$content);
+        fclose($fp);
+        shell_exec( 'cd /var/www/html/pngtimeaccess && git reset –hard HEAD && git pull' );
+    }
 
 ?>
